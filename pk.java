af9cd61753cca8a4375eec769f0cac36b3c09077
praveen <kodekalpraveena@gmail.com>
2023-06-30 12:08:28 +0530

    addded comment line

--- pk.java
+++ pk.java
@@ -1,7 +1,7 @@
 public class gitPull{
   public static void main(String[] args){
 	int i=10;
-
+    // Loaded in git branch
     String name="praveen";
     System.out.println("Pull commited");
   }
