81b6edefac3903d39412bc73526083b1ce6ad45c
praveen <kodekalpraveena@gmail.com>
2023-06-30 12:32:01 +0530

    second line commit

--- pk.java
+++ pk.java
@@ -2,6 +2,7 @@ public class gitPull{
   public static void main(String[] args){
 	int i=10;
     // Loaded in git branch
+	// second branch commit
     String name="praveen";
     System.out.println("Pull commited");
   }
